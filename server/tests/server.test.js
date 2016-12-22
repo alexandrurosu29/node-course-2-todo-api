bc5fd103450981504364522e84929aa150e1dc4d
Alex <alexandru.rosu@osf-global.com>
2016-12-22 16:41:07 +0200

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -117,3 +117,43 @@ describe('GET /todos/:id', () => {
 			.end(done);
 	});
 });	
+
+describe('DELETE /todos/:id', () => {
+	it('should remove a todo', (done) => {
+		request(app)
+			.delete(`/todos/${todos[1]._id.toHexString()}`)
+			.expect(200)
+			// custom expect
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(todos[1].text);
+			})
+			.end((err, res) => {
+				if(err) {
+					return done(err);
+				}
+
+				Todo.findById(todos[1]._id).then((todo) => {
+					expect(todo).toNotExist();
+					done();
+				}).catch((e) => {
+					done(e);
+				})
+			});
+	});
+
+	it('should remove a 404 if todo not found', (done) => {
+		let hexId = new ObjectID();
+
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(404)
+			.end(done);
+	});
+
+	it('should return 404 if object id is invalid', (done) => {
+		request(app)
+			.delete(`/todos/123abc`)
+			.expect(404)
+			.end(done);
+	});
+});
