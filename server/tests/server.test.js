3398273a69afd9e9df86166348917dc7e595af22
Alex <alexandru.rosu@osf-global.com>
2016-12-22 11:42:23 +0200

    Test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+// runs before any test case
+beforeEach((done) => {
+	// remove all todos from db
+	Todo.remove({}).then(() => {
+		done();
+	})
+});
+
+describe('POST /todos', () => {
+	it('should create a new todo', (done) => {
+		let text = 'Test todo text';
+		request(app)
+			.post('/todos')
+			.send({
+				text
+			})
+			.expect(200)
+			.expect((res) =>{
+				expect(res.body.text).toBe(text);
+			})
+			.end((err, res) => {
+				if(err) {
+					return done(err);
+				}
+
+				Todo.find().then((todos) => {
+					expect(todos.length).toBe(1);
+					expect(todos[0].text).toBe(text);
+					done();
+				}).catch((e) => {
+					done(e);
+				})
+			});
+	});
+
+	it('should not create todo with invalid body data', (done) => {
+		request(app)
+			.post('/todos')
+			.send({})
+			.expect(400)
+			.end((err, res) => {
+				if(err) {
+					return done(err);
+				}
+
+				Todo.find().then((todos) => {
+					expect(todos.length).toBe(0);
+					done();
+				}).catch((e) => {
+					done(e);
+				})
+			})
+	});
+});
