352e89aa797be1acab20006fc3b47d61494f0c48
Alex <alexandru.rosu@osf-global.com>
2016-12-26 09:16:49 +0200

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -95,6 +95,26 @@ UserSchema.statics.findByToken = function(token) {
 	});
 }
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  let User = this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if(res) {
+          resolve(user);
+        } else {
+          reject(user);
+        }
+      });
+    })
+  });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User}
